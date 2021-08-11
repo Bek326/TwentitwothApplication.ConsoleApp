d9a7d010d20b5bf4a69346590742c31878fc89b5
Name <email@example.ru>
2021-08-11 17:32:02 +0600

    Добавьте файлы проекта.

---
+++ TwentitwothApplication.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TwentitwothApplication.ConsoleApp
+{
+    class Program
+    {
+        public static void Folder()
+        {
+
+            DirectoryInfo dirName = new DirectoryInfo(@"C:\Users\TheDarkKnight\Downloads\TestFolder");
+            if (dirName.Exists)
+            {
+                long sum = 0;
+                var dirs = dirName.GetDirectories().ToList();
+                dirs.Add(dirName);
+                foreach (var dir in dirs)
+                {
+                    var files = dir.GetFiles();
+                    foreach (var file in files)
+                    {
+                        sum += file.Length;
+                    }
+                }
+                Console.WriteLine($"Объем:{sum}");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Folder();
+            Console.ReadKey();
+        }
+    }
+}
